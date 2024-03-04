ab902c8965e72aef5ce83cde073f6c273b1c4669
Socrates Mejia <socrates@asadigital.net>
2024-03-04 19:05:33 +0900

    removed iFrame

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       </header>
       <div>
         <iframe allow="autoplay" sandbox="allow-modals allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-popups-to-escape-sandbox" id="9Tdqg5" class="embed_frame" loading="lazy" src="https://m.shortstack.page/9Tdqg5?embed=3&script=1" style="border:none;min-width:100%;height:1605px" allowpaymentrequest allowfullscreen data-hj-allow-iframe></iframe>
-        <script>!function(){var e=document.getElementById("campaign-embed-code");e||((e=document.createElement("SCRIPT")).src="https://d1m2uzvk8r2fcn.cloudfront.net/scripts/embed-code/20230927/embed.min.js",e.id="campaign-embed-code",document.body.appendChild(e))}()</script>
+        
       </div>
     </div>
   );
