7962f9c15af082c39c5e1206ef99a18668d2011d
Socrates Mejia <socrates@asadigital.net>
2024-03-04 17:55:19 +0900

    added iFrame
    iFrame

--- src/App.js
+++ src/App.js
@@ -9,15 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Or something else
-        </a>
       </header>
+      <div>
+        <iframe allow="autoplay" sandbox="allow-modals allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-popups-to-escape-sandbox" id="9Tdqg5" class="embed_frame" loading="lazy" src="https://m.shortstack.page/9Tdqg5?embed=3&script=1" style="border:none;min-width:100%;height:1605px" allowpaymentrequest allowfullscreen data-hj-allow-iframe></iframe>
+        <script>!function(){var e=document.getElementById("campaign-embed-code");e||((e=document.createElement("SCRIPT")).src="https://d1m2uzvk8r2fcn.cloudfront.net/scripts/embed-code/20230927/embed.min.js",e.id="campaign-embed-code",document.body.appendChild(e))}()</script>
+      </div>
     </div>
   );
 }
